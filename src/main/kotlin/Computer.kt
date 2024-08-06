068f2dda58b13e27162d7e1f8a6ec1e9c9dfbf5d
Ryan Christensen <a023107782@usu.edu>
2024-08-06 13:42:49 -0600

    Update roms directory, updates to computer

--- src/main/kotlin/Computer.kt
+++ src/main/kotlin/Computer.kt
@@ -5,54 +5,54 @@ import java.util.concurrent.ScheduledFuture
 import java.util.concurrent.TimeUnit
 
 class Computer {
-//    private val cpu = CPU()
-//    private val memory = arrayOf(RAM(), ROM())
-//    private val display = Display()
-//    private val instructions = InstructionFactory(cpu, memory, display)
-//
-//    private val executor = Executors.newSingleThreadScheduledExecutor()
-//    private var cpuFuture: ScheduledFuture<*>? = null
-//
-//    fun start() {
-//        memory[1].loadROM()
-//        startEmulation()
-//    }
-//
-//    private fun startEmulation() {
-//        val runnable = Runnable {
-//            val firstByte = memory[1].read(cpu.counter.toInt())
-//            val secondByte = memory[1].read(cpu.counter.toInt() + 1)
-//
-//            if (firstByte == 0.toUByte() && secondByte == 0.toUByte()) {
-//                stop()
-//            } else {
-//                instructions.executeInstruction(firstByte, secondByte)
-//            }
-//        }
-//
-//        cpuFuture = executor.scheduleAtFixedRate(
-//            runnable,
-//            0,
-//            500L, // repeat frequency - every 500 ms
-//            TimeUnit.MILLISECONDS
-//        )
-//    }
-//
-//    private fun stop() {
-//        cpuFuture?.cancel(true)
-//
-//        // Shut down the executor and wait for ongoing tasks to complete
-//        executor.shutdown()
-//        try {
-//            if (!executor.awaitTermination(1, TimeUnit.SECONDS)) {
-//                executor.shutdownNow()
-//                if (!executor.awaitTermination(1, TimeUnit.SECONDS)) {
-//                    System.err.println("Executor did not terminate in time")
-//                }
-//            }
-//        } catch (interruptedException: InterruptedException) {
-//            executor.shutdownNow()
-//            Thread.currentThread().interrupt()
-//        }
-//    }
+    private val cpu = CPU()
+    private val memory = arrayOf(RAM(), ROM())
+    private val display = Display()
+    private val instructions = FactoryForInstructions(cpu, memory, display)
+
+    private val executor = Executors.newSingleThreadScheduledExecutor()
+    private var cpuFuture: ScheduledFuture<*>? = null
+
+    fun start() {
+        memory[1].loadROM()
+        startEmulation()
+    }
+
+    private fun startEmulation() {
+        val runnable = Runnable {
+            val firstByte = memory[1].read(cpu.counter.toInt())
+            val secondByte = memory[1].read(cpu.counter.toInt() + 1)
+
+            if (firstByte == 0.toUByte() && secondByte == 0.toUByte()) {
+                stop()
+            } else {
+                instructions.execute(firstByte, secondByte)
+            }
+        }
+
+        cpuFuture = executor.scheduleAtFixedRate(
+            runnable,
+            0,
+            500L, // repeat frequency - every 500 ms
+            TimeUnit.MILLISECONDS
+        )
+    }
+
+    private fun stop() {
+        cpuFuture?.cancel(true)
+
+        // Shut down the executor and wait for ongoing tasks to complete
+        executor.shutdown()
+        try {
+            if (!executor.awaitTermination(1, TimeUnit.SECONDS)) {
+                executor.shutdownNow()
+                if (!executor.awaitTermination(1, TimeUnit.SECONDS)) {
+                    System.err.println("Executor did not terminate in time")
+                }
+            }
+        } catch (interruptedException: InterruptedException) {
+            executor.shutdownNow()
+            Thread.currentThread().interrupt()
+        }
+    }
 }
--- src/main/kotlin/FactoryForInstructions.kt
+++ src/main/kotlin/FactoryForInstructions.kt
@@ -1,5 +1,41 @@
 package org.example
 
-class FactoryForInstructions {
+class FactoryForInstructions(
+    private val cpu: CPU,
+    private val memory: Array<Memory>,
+    private val display: Display
+) {
+    private val instructions: Map<String, InstructionTemplate> = mapOf(
+        "0" to Store(this.cpu, this.memory, this.display),
+        "1" to Add(this.cpu, this.memory, this.display),
+        "2" to Sub(this.cpu, this.memory, this.display),
+        "3" to Read(this.cpu, this.memory, this.display),
+        "4" to Write(this.cpu, this.memory, this.display),
+        "5" to Jump(this.cpu, this.memory, this.display),
+        "6" to ReadKeyboard(this.cpu, this.memory, this.display),
+        "7" to SwitchMemory(this.cpu, this.memory, this.display),
+        "8" to SkipEqual(this.cpu, this.memory, this.display),
+        "9" to SkipNotEqual(this.cpu, this.memory, this.display),
+        "A" to SetA(this.cpu, this.memory, this.display),
+        "B" to SetT(this.cpu, this.memory, this.display),
+        "C" to ReadT(this.cpu, this.memory, this.display),
+        "D" to ConvertToBaseTen(this.cpu, this.memory, this.display),
+        "E" to ConvertByteToASCII(this.cpu, this.memory, this.display),
+        "F" to Draw(this.cpu, this.memory, this.display)
+    )
 
+    fun execute(b1: UByte, b2: UByte) {
+        val n1 = convertUByteToHex(b1)
+        val n2 = convertUByteToHex(b2)
+        val instructionValue = n1.chunked(1)[0]
+
+        val instruction = instructions[instructionValue]
+            ?: throw IllegalArgumentException("Error: Invalid Instruction")
+
+        instruction.execute(n1, n2)
+    }
+
+    private fun convertUByteToHex(byte: UByte): String {
+        return String.format("%02X", byte.toByte())
+    }
 }
