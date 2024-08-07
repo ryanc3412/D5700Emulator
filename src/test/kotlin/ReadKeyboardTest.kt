209ea1ac9a7ba43b538b3db0d045fafff0c08558
Ryan Christensen <a023107782@usu.edu>
2024-08-06 23:09:11 -0600

    read keyboard tests

--- src/test/kotlin/ReadKeyboardTest.kt
+++ src/test/kotlin/ReadKeyboardTest.kt
@@ -1,4 +1,21 @@
 package org.example
 
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
 class ReadKeyboardTest {
+    val cpu = CPU()
+    val array = arrayOf(RAM(), ROM())
+    val display = Display()
+    val keyboard = ReadKeyboard(cpu, array, display)
+
+    @Test
+    fun testSplit() {
+        assertEquals(true,keyboard.hexadecimalChecker("1A"))
+        assertEquals(false,keyboard.hexadecimalChecker("5P"))
+        assertEquals(true,keyboard.hexadecimalChecker("9D"))
+        assertEquals(false,keyboard.hexadecimalChecker("-45"))
+        assertEquals(true,keyboard.hexadecimalChecker("FFFF"))
+        assertEquals(true,keyboard.hexadecimalChecker("023"))
+    }
 }
