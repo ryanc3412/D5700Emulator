812e7b7b9b49a8c138e2423f96ce9f0ef76007be
Ryan Christensen <a023107782@usu.edu>
2024-08-06 23:09:31 -0600

    setA tests

--- src/test/kotlin/SetATest.kt
+++ src/test/kotlin/SetATest.kt
@@ -1,4 +1,19 @@
 package org.example
 
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
 class SetATest {
+
+    @Test
+    fun testSetAddressOperation() {
+        val processor = CPU()
+        val memoryUnits = arrayOf(RAM(), ROM())
+        val screen = Display()
+        val setAddress = SetA(processor, memoryUnits, screen)
+
+        assertEquals(0u, processor.address)
+        setAddress.execute("A2", "55")
+        assertEquals(597u, processor.address)
+    }
 }
